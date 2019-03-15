965b9beb4119cb97a9b927cb7a187746d297798c
Alexander Konovalov <alexk@mcs.st-andrews.ac.uk>
2019-03-15 23:25:48 +0000

    Run package tests in three configurations instead of one

--- ci.sh
+++ ci.sh
@@ -46,7 +46,7 @@ then
   fi
   make
 else
-  echo "No building required for $PKG"
+  echo "No building required for $PKG_NAME"
 fi
 
 # set up a custom GAP root containing only this package, so that
@@ -59,8 +59,45 @@ ln -s $PWD gaproot/pkg/
 # start GAP with custom GAP root, to ensure correct package version is loaded
 GAP="/home/gap/inst/${GAPDIRNAME}/bin/gap.sh -l $PWD/gaproot; --quitonbreak -q"
 
-# Run package test
+echo ""
+echo "######################################################################"
+echo "#"
+echo "# TEST WITH DEFAULT PACKAGES, LOADED AT GAP STARTUP"
+echo "#"
+echo "######################################################################"
+echo ""
 $GAP <<GAPInput
+Read("/home/gap/travis/ci.g");
+if TestOnePackage(LowercaseString(GetNameFromPackageInfo("PackageInfo.g"))) <> true then
+    FORCE_QUIT_GAP(1);
+fi;
+QUIT_GAP(0);
+GAPInput
+
+echo ""
+echo "######################################################################"
+echo "#"
+echo "# TEST WITHOUT PACKAGES, EXCEPT REQUIRED BY GAP (using -A option)"
+echo "#"
+echo "######################################################################"
+echo ""
+$GAP -A <<GAPInput
+Read("/home/gap/travis/ci.g");
+if TestOnePackage(LowercaseString(GetNameFromPackageInfo("PackageInfo.g"))) <> true then
+    FORCE_QUIT_GAP(1);
+fi;
+QUIT_GAP(0);
+GAPInput
+
+echo ""
+echo "######################################################################"
+echo "#"
+echo "# TEST WITH ALL PACKAGES LOADED (using LoadAllPackages() command)"
+echo "#"
+echo "######################################################################"
+echo ""
+$GAP <<GAPInput
+LoadAllPackages();
 Read("/home/gap/travis/ci.g");
 if TestOnePackage(LowercaseString(GetNameFromPackageInfo("PackageInfo.g"))) <> true then
     FORCE_QUIT_GAP(1);
